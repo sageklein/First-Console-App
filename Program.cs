f4354414969b94ca996a6ff58dc3a6d14430e2bc
Sage Klein <sageklein@gmail.com>
2020-11-11 10:08:56 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! I'm glad to meet you.");
+        }
+    }
+}
